f1768de5579e990248366ddf64354a46c749356d
ypmo <ypmo.dev@gmail.com>
2026-09-11 02:15:52 +0200

    style: adjust general styles

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,6 +1,18 @@
-document.querySelectorAll('[data-private]').forEach(el => {
-  el.addEventListener('click', e => {
-    e.preventDefault();
-    openLoginModal();
-  });
+const modal = document.getElementById("accessModal");
+const closeModal = document.querySelector(".modal__close");
+
+const privateButtons = document.querySelectorAll('[data-private="true"]');
+
+privateButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        modal.classList.add("is-open");
+        modal.setAttribute("aria-hidden", "false");
+    });
+});
+
+closeModal.addEventListener("click", () => {
+    modal.classList.remove("is-open");
+    modal.setAttribute("aria-hidden", "true");
 });
